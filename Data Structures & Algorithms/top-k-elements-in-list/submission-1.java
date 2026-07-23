d7b22f4860643acb3c896b6459506ade4a1d8768
boiCodes <pranatkheria@gmail.com>
2026-07-23 22:27:31 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap <Integer,Integer> hashmap = new HashMap<>();
+        for (int i = 0 ; i<nums.length ; i++){
+            if (!hashmap.containsKey(nums[i])){
+                hashmap.put(nums[i],1);
+            }
+            else
+                hashmap.put(nums[i],hashmap.get(nums[i])+1);
+        }
+
+        int[] allvalues = new int[hashmap.size()];
+        int[] keyArray = new int[hashmap.size()];
+
+        int index = 0;
+        for (int key : hashmap.keySet()){
+            keyArray[index] = key;
+            allvalues[index] = hashmap.get(key);
+            index++;
+        }
+
+        for (int i = 0 ; i<allvalues.length ; i++){
+            for (int j = 0 ; j<allvalues.length-i-1 ; j++){
+                if(allvalues[j] > allvalues[j+1]){
+
+                    int tempValue = allvalues[j];
+                    allvalues[j] = allvalues[j+1];
+                    allvalues[j+1] = tempValue;
+
+                    int temp = keyArray[j];
+                    keyArray[j] = keyArray[j+1];
+                    keyArray[j+1]=temp;
+                }
+            }
+        }
+
+        int[] resultSet = new int[k];
+        int count = 0;
+        for (int i = keyArray.length-1 ; i>=0 && count<k; i--){
+            if(count == k){
+                break;
+            }
+            resultSet[count++] = keyArray[i];
+        }
+
+        return resultSet;
+
+    }
+}
